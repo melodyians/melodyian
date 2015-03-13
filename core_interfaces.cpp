453c7615a8ac5ad3842f754db12587a34db96fde
Jim Fingal <jim.fingal@gmail.com>
2015-03-13 15:49:18 -0700

    remove robot_input, move impl to core_interfaces

--- core_interfaces.cpp
+++ core_interfaces.cpp
@@ -1,5 +1,5 @@
 #include "core_interfaces.h"
-
+#include "helper_midi.h"
 
 Behavior::Behavior() {
     clearCurrentBehavior();
@@ -30,3 +30,33 @@ void Behavior::incrementTimer(unsigned short dt) {
 void Behavior::decrementTimer(unsigned short dt) {
     timer =  timer - dt;
 }
+
+
+InputValues::InputValues(byte lowest_cc) {
+    offset = lowest_cc;
+    int array_size = (127 - lowest_cc) + 1;
+    input_values = new byte[array_size];
+
+    for (int i = 0; i < array_size; i++) {
+        input_values[i] = (byte) 0;
+    }
+ 
+};
+
+byte InputValues::getCCIndex(byte control_number) {
+    return control_number - offset;
+};
+
+byte InputValues::getValue(byte control_number) {
+    return input_values[getCCIndex(control_number)];
+};
+
+void InputValues::storeInput(byte control_number, byte value) {
+
+    if (DEBUG) {
+        MidiCC::WriteMidiOut(control_number,  value);
+        MidiCC::WriteMidiOut(getCCIndex(control_number),  value);
+    }
+
+    input_values[getCCIndex(control_number)] = value;
+};
